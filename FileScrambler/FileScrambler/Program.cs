4381162f8fc57eefc2c5477f049a8e294e8b9637
Tsubasa <Catsterrace@gmail.com>
2021-03-04 11:10:59 -0500

    Moved ScrambleFiles method into its own class

--- FileScrambler/FileScrambler/Program.cs
+++ FileScrambler/FileScrambler/Program.cs
@@ -34,71 +34,13 @@ namespace FileScrambler
 
             if (verify == "Y")
             {
-                Scramble(programDirectory,filePath, files);
+                Scramble.ScrambleFiles(programDirectory,filePath, files);
             }
             if(verify == "N")
             {
                 Console.Write("Enter to close");
                 Console.ReadLine();
             }
-        }
-
-
-        static void Scramble(string programDirectory, string[] filePath, List<string> files)
-        {
-            int i;
-            List<string> Scrambledfiles;
-            List<string> iFiles;
-            List<string> ext;
-            Random rnd = new Random();
-
-            iFiles = new List<string>(files);
-            Scrambledfiles = new List<string>();
-            ext = new List<string>();
-
-            while (iFiles.Count > 0)
-            {
-                Scrambledfiles.Add(iFiles[i = rnd.Next(iFiles.Count)]);
-                iFiles.RemoveAt(i);
-            }
-
-            for (i = 0; i < Scrambledfiles.Count; i++)
-            {
-                Console.WriteLine(Scrambledfiles[i]);
-            }
-
-            for (i = 0; i < Scrambledfiles.Count; i++)
-            {
-                FileInfo FI = new FileInfo(filePath[i]);
-                string file = Scrambledfiles[i];
-                                
-                ext.Add(Path.GetExtension(filePath[i]));
-
-                int j = Scrambledfiles[i].LastIndexOf(".");
-
-                if (j != -1)
-                {
-                    file = file.Substring(0, j);
-                }
-
-                FI.MoveTo(programDirectory + @"\" + file, true);
-                Console.WriteLine(programDirectory + @"\" + file);
-            }
-
-            for (i = 0; i < files.Count; i++)
-            {
-                string file = Scrambledfiles[i];
-                int j = Scrambledfiles[i].LastIndexOf(".");
-
-                if (j != -1)
-                {
-                    file = file.Substring(0, j);
-                }
-
-                FileInfo FI = new FileInfo(programDirectory + @"\" + file);
-                FI.MoveTo(programDirectory + @"\" + file + ext[i],true);              
-            }
-            Console.ReadLine();
         }
     }
 }
---
+++ FileScrambler/FileScrambler/Scramble.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileScrambler
+{
+    public class Scramble
+    {
+        public static void ScrambleFiles(string programDirectory, string[] filePath, List<string> files)
+        {
+            int i;
+            List<string> Scrambledfiles;
+            List<string> iFiles;
+            List<string> ext;
+            Random rnd = new Random();
+
+            iFiles = new List<string>(files);
+            Scrambledfiles = new List<string>();
+            ext = new List<string>();
+
+            while (iFiles.Count > 0)
+            {
+                Scrambledfiles.Add(iFiles[i = rnd.Next(iFiles.Count)]);
+                iFiles.RemoveAt(i);
+            }
+
+            for (i = 0; i < Scrambledfiles.Count; i++)
+            {
+                Console.WriteLine(Scrambledfiles[i]);
+            }
+
+            for (i = 0; i < Scrambledfiles.Count; i++)
+            {
+                FileInfo FI = new FileInfo(filePath[i]);
+                string file = Scrambledfiles[i];
+
+                ext.Add(Path.GetExtension(filePath[i]));
+
+                int j = Scrambledfiles[i].LastIndexOf(".");
+
+                if (j != -1)
+                {
+                    file = file.Substring(0, j);
+                }
+
+                FI.MoveTo(programDirectory + @"\" + file, true);
+                Console.WriteLine(programDirectory + @"\" + file);
+            }
+
+            for (i = 0; i < files.Count; i++)
+            {
+                string file = Scrambledfiles[i];
+                int j = Scrambledfiles[i].LastIndexOf(".");
+
+                if (j != -1)
+                {
+                    file = file.Substring(0, j);
+                }
+
+                FileInfo FI = new FileInfo(programDirectory + @"\" + file);
+                FI.MoveTo(programDirectory + @"\" + file + ext[i], true);
+            }
+            Console.ReadLine();
+        }
+    }
+}
